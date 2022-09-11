04a1bcbb7f7d0df6b152b93ae6c94107b808c550
Ivan Cheng <ivancheng1214@gmail.com>
2022-09-11 12:03:07 -0400

    fix: app component unit test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,22 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, waitForElementToBeRemoved } from '@testing-library/react';
+
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders app container with loading text', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+
+  const appContainerElem = screen.getByTestId('app');
+  const loadingElem = screen.getByTestId('loading');
+
+  expect(appContainerElem).toBeInTheDocument();
+  expect(loadingElem).toBeInTheDocument();
+});
+
+test('renders app content after fetching data', async () => {
+  render(<App />);
+
+  await waitForElementToBeRemoved(() => screen.queryByText(/Loading/i));
+
+  const contentElem = screen.getByTestId('content');
+  expect(contentElem).toBeInTheDocument();
 });
